37cfb69d85629ee40301ed7ec1ee231af50eab9d
mhdhasan8 <hasanmuhammad197@gmail.com>
2022-05-12 23:36:13 +0700

    add started page

--- lib/pages/started_page.dart
+++ lib/pages/started_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class GetStartedPage extends StatelessWidget {
   @override
@@ -13,6 +14,81 @@ class GetStartedPage extends StatelessWidget {
                 fit: BoxFit.fill,
               ),
             ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 50, right: 30, left: 30),
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Text(
+                'Build Your Language \nFuture Career Like \na Master',
+                style: GoogleFonts.poppins(
+                  color: Colors.white,
+                  fontSize: 30,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'UPT Bahasa Polije',
+                style: GoogleFonts.poppins(
+                  color: Colors.white,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 430),
+                  child: Container(
+                    width: 200,
+                    height: 45,
+                    child: TextButton(
+                      style: TextButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(66),
+                        ),
+                        backgroundColor: Color(0xffffffff),
+                      ),
+                      onPressed: () {},
+                      child: Text(
+                        'Get Started',
+                        style: GoogleFonts.poppins(
+                          color: Color(0xff08AFE6),
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 16),
+                  child: Container(
+                    width: 200,
+                    height: 45,
+                    child: OutlineButton(
+                      onPressed: () {},
+                      borderSide: BorderSide(color: Colors.white),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(66),
+                      ),
+                      child: Text(
+                        'Sign In',
+                        style: GoogleFonts.poppins(
+                          color: Colors.white,
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ]),
           )
         ],
       ),
